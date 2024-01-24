9c136c3b28e87f8020e33a0ae81736d4e8dad401
Talmage Kelley III <talkelley3@outlook.com>
2024-01-24 15:43:47 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import pandas
+from pyzscaler.zia import ZIA
+def main():
+    #main function
+    print("Welcome to The Zscaler URL Management Interface!")
+if __name__== "__main__":
+    main()    
